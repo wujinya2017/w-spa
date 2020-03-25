72e1821ae0106168111216fd21bdafbda4a5176f
wujinya <2601519403@qq.com>
2020-03-25 09:23:18 +0800

    dlg

---
+++ dlg-goto/dlg-goto.js
@@ -0,0 +1,39 @@
+var $dlgGoto = (function(){    
+  function show(conf){ 
+    var $dlg = $(''
+       + '<div class="notepad-dlg-mask notepad-dlg-goto">'
+       + '<div class="dialogbox notepad-dlgbox">'
+       + '<div class="notepad-dlg-titlebar">'
+       + '<p class="title">转到指定行</p>'
+       + '<span class="close-btn">✖</span>'
+       + '</div>'
+       + '<div class="main notepad-dlg-main">'                                                   + '<label for="">行号(L):</label><br>'
+       + '<input class="txt-line-num" type="text" autofocus><br>'
+       + '<input class="btn-goto btn" type="button" value="转到">'
+       + '<input class="btn-cancel btn" type="button" value="取消">'
+       + '</div>'
+       + '</div>'
+       + '</div>' 
+    );
+     $dlg.css({
+        height:'50px',
+        width:'150px'
+     })
+     var  cfg = {
+            container:'body',
+            num : 6,
+            title:'同意',
+            onClick:null
+          };
+    $(cfg.container).append($dlg);
+    $.extend(cfg,conf);//追加到最新配置
+    num = cfg.num;
+    $dlg.val(cfg.title + '(' + cfg.num + 's)');//按钮初始值
+    
+   //2.event bind
+    $dlg.click(cfg.onClick);
+  }
+  return {
+    show : show
+  }
+}());
